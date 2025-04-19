bdab2cc73d386a0d875c847c93bd281c2f8ab5ca
saulamsal <saulamsal@gmail.com>
2025-04-19 18:43:14 -0400

    wip

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,10 @@
 import '../global.css';
+import { useState, useEffect } from 'react'; 
 
 import { Stack } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
+import AnimatedSplash from "react-native-animated-splash-screen"; 
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
@@ -10,21 +12,32 @@ export const unstable_settings = {
 };
 
 export default function RootLayout() {
+  const [isLoaded, setIsLoaded] = useState(false); 
 
-
-  
-
-
-
+  useEffect(() => {
+    // Simulate loading task
+    setTimeout(() => {
+      setIsLoaded(true);
+    }, 1000); 
+  }, []);
 
   return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-       <BottomSheetModalProvider>
-      <Stack>
+    <AnimatedSplash
+      translucent={true} 
+      isLoaded={isLoaded}
+      logoImage={{uri: "https://upload.wikimedia.org/wikipedia/commons/5/57/X_logo_2023_%28white%29.png"}} 
+      backgroundColor={"#000000"} 
+      logoHeight={150}
+      logoWidth={150}
+    >
+      <GestureHandlerRootView style={{ flex: 1 }}>
+         <BottomSheetModalProvider>
+        <Stack>
         <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ title: 'Modal', presentation: 'modal' }} />
       </Stack>
       </BottomSheetModalProvider>
-    </GestureHandlerRootView>
+      </GestureHandlerRootView>
+    </AnimatedSplash>
   );
 }
--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -1,13 +1,16 @@
+import { useState } from "react"
 import { TouchableOpacity, Text, View } from "react-native"
 import Like from "~/assets/svg/like.svg"
+import LikeFilled from "~/assets/svg/like-filled.svg"
 
 
 function LikeButton({ iconColor, textColor }: { iconColor: string, textColor: string }) {
+    const [isLiked, setIsLiked] = useState(false)
     return (
 
-        <TouchableOpacity className="flex-row items-center gap-1.5">
-            <Like width={18} height={18} fill={iconColor} />
-            <Text className={textColor}>100</Text>
+        <TouchableOpacity className="flex-row items-center gap-1.5" onPress={() => setIsLiked(!isLiked)}>
+            {isLiked ? <LikeFilled width={18} height={18} fill={"#f91880"} /> : <Like width={18} height={18} fill={iconColor} />}
+            <Text className={textColor} style={{ color: isLiked ? "#f91880" : textColor }}>100</Text>
         </TouchableOpacity>
     )
 }
